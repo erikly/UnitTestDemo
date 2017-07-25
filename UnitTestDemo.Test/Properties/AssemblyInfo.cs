55ffe56a77708e29af06255f971538cee32ebce8
Erik Lydersen <erikly@gmail.com>
2017-07-25 12:05:15 +0200

    Fixed everything

--- UnitTestDemo.Test/Properties/AssemblyInfo.cs
+++ UnitTestDemo.Test/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UnitTest.Test")]
+[assembly: AssemblyTitle("UnitTestDemo.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTest.Test")]
+[assembly: AssemblyProduct("UnitTestDemo.Test")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("e005dd9b-d417-4f1d-8832-87f7ee4f4123")]
+[assembly: Guid("10a1b4f7-39aa-473b-b35f-ea9363b92fb3")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
